2cd3e6340b4db10f74db298b74e56d5c624dfd11
Pinlin <pinlin168@gmail.com>
2021-04-12 16:27:25 +0800

    improve: make sure LUT intensity is always within valid range

--- framework/Source/Operations/LookupFilter.swift
+++ framework/Source/Operations/LookupFilter.swift
@@ -1,7 +1,16 @@
 // PictureInput isn't defined yet on Linux, so  this operation is inoperable there
 #if !os(Linux)
 public class LookupFilter: BasicOperation {
-    public var intensity: Float = 1.0 { didSet { uniformSettings["intensity"] = intensity } }
+    public var intensity: Float = 1.0 {
+        didSet {
+            if intensity < 0 || intensity > 1.0 {
+                assertionFailure("LookupFilter intensity:\(intensity) is out of valid range [0, 1.0]")
+                intensity = min(max(intensity, 0), 1.0)
+                return
+            }
+            uniformSettings["intensity"] = intensity
+        }
+    }
     public var lookupImage: PictureInput? { // TODO: Check for retain cycles in all cases here
         didSet {
             lookupImage?.addTarget(self, atTargetIndex: 1)
